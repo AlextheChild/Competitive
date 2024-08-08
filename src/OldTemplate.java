82f07a636bc441c779d9da763f8abee5d5cc21f5
AlextheChild <alexjun2008@gmail.com>
2024-08-07 23:15:45 -0400

    uwu owo new format

--- src/OldTemplate.java
+++ src/OldTemplate.java
@@ -3,10 +3,11 @@ import java.io.*;
 public class OldTemplate {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new FileReader(new File("file.in")));
+        PrintWriter out = new PrintWriter("file.out");
         in.readLine();
 
-        PrintWriter out = new PrintWriter("file.out");
         out.println();
+
         in.close();
         out.close();
     }
