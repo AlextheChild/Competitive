918b786c29fafe51a09b5d321d072ccbffdcac97
AlextheChild <alexjun2008@gmail.com>
2024-08-08 23:34:45 -0400

    nightmare nightmare nightmare

--- src/silver/Roads.java
+++ src/silver/Roads.java
@@ -10,8 +10,7 @@ public class Roads {
     static ArrayList<int[]> unusedRoads = new ArrayList<int[]>();
 
     public static void main(String[] args) throws Exception {
-        BufferedReader in = new BufferedReader(new FileReader(new File("file.in")));
-        PrintWriter out = new PrintWriter("file.out");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(in.readLine());
         parents = new int[n];
         for (int i = 0; i < n; i++) {
@@ -32,30 +31,20 @@ public class Roads {
         }
 
         // add better roads
-        // HashSet<Integer> linkedRoads = new HashSet<Integer>();
-        // linkedRoads.add(parents[0]);
-        out.println(unusedRoads.size());
+        System.out.println(unusedRoads.size());
         for (int i = 0; i < n; i++) {
-            // if (!linkedRoads.contains(parents[i])) {
             if (find(0) != find(i)) {
-                for (int j : parents) {
-                    System.out.print(j);
-                }
-                System.out.println();
-                out.println("" + (unusedRoads.get(0)[0] + 1) +
+                System.out.println("" + (unusedRoads.get(0)[0] + 1) +
                         " " +
                         (unusedRoads.get(0)[1] + 1) +
                         " " + (parents[0] + 1) +
                         " " + (i + 1));
                 unusedRoads.remove(0);
                 union(0, i);
-                // linkedRoads.add(i);
             }
         }
 
-        out.println();
         in.close();
-        out.close();
     }
 
     public static int find(int node) {
