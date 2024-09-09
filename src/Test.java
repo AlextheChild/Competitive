226b5567e26f7093bec74d34b1c8eb1cc4f9c545
AlextheChild <alexjun2008@gmail.com>
2024-09-09 19:26:09 -0400

    uwu

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,4 @@
 public class Test {
     public static void main(String[] args) throws Exception {
-        for (int t = 0; t < 100; t++) {
-            System.out.println((((t / 10) % 2 == 0 ? t % 11 : 10 - t % 11) / 10) * 12);
-        }
     }
 }
